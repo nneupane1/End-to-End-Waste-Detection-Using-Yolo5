80be3675189eacf54ca0c8410a1fa05443081157
nneupane1 <nneupane1@gmail.com>
2023-09-27 23:37:21 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Nischal Neupane',
+    author_email= 'nneupane1@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
